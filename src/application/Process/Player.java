ba878f26490c9fb88efd45d8817fffd6b28dee7a
Wattanai Tipsathaporn <foamrock@gmail.com>
2018-11-18 17:21:07 +0700

    Add currentLocation to Players properties

--- src/application/Process/Player.java
+++ src/application/Process/Player.java
@@ -1,14 +1,27 @@
 package application.Process;
 
+
 public class Player {
 	private String name;
+	private Location currentLocation;
 	
 	public Player(String name) {
 		this.name = name;
+		int x = (int) ((Math.random() * ((4 - 0) + 1)) + 0);
+		int y = (int) ((Math.random() * ((4 - 0) + 1)) + 0);
+		this.currentLocation = new Location(x,y);
 	}
 
 	public String getName() {
 		return name;
+	}
+
+	public Location getCurrentLocation() {
+		return currentLocation;
+	}
+
+	public void setCurrentLocation(Location currentLocation) {
+		this.currentLocation = currentLocation;
 	}
 
 	public void setName(String name) {
