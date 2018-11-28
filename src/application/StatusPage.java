39f2c2603341de56b45455a7bc625e2a593d0e3c
Worawit Jitpakdeebodin <retpoc4391@hotmamil.com>
2018-11-28 15:11:41 +0700

    add attribute p1,p2 to main

--- src/application/StatusPage.java
+++ src/application/StatusPage.java
@@ -3,24 +3,27 @@ package application;
 import javafx.scene.shape.*;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.scene.Group;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 public class StatusPage extends VBox{
 	Label pt;
-	Rectangle r;
-	Group g;
+
 	public StatusPage() {
 		// TODO Auto-generated constructor stub
 		this.setSpacing(15);
 		pt = new Label("Player's Turn:");
 		pt.setFont(Font.font("Serif",FontWeight.NORMAL,24));
 		this.getChildren().add(pt);
-		r = new Rectangle(10,10,50,50);
-		g = new Group();
-		g.getChildren().add(r);
-		this.getChildren().add(g);
+	
+	}
+	public void setPlayerName(String p1,String p2) {
+		Text pr1 = new Text(p1);
+		Text pr2 = new Text(p2);
+		this.getChildren().addAll(pr1,pr2);
 	}
 
 }
